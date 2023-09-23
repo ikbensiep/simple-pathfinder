132666b0a9cb6628a89b15be902554222b6a71db
ikbensiep <ikbensiep>
2023-09-24 00:59:20 +0200

    (debug) spawn player at mouseclick location

--- js/modules/game.js
+++ js/modules/game.js
@@ -18,11 +18,13 @@ export default class Game {
       pressed: false
     }
 
-
+    // spawn player at mouse click location
     this.canvas.addEventListener('mousedown', (e) => {
       this.mouse.x = e.offsetX;
       this.mouse.y = e.offsetY;
       this.mouse.pressed = true;
+      this.player.origin.x = this.mouse.x
+      this.player.origin.y = this.mouse.y
     })
     
     this.canvas.addEventListener('mouseup', (e) => {
@@ -35,6 +37,8 @@ export default class Game {
       if(this.mouse.pressed) {
         this.mouse.x = e.offsetX;
         this.mouse.y = e.offsetY;
+        this.player.origin.x = this.mouse.x
+        this.player.origin.y = this.mouse.y
       }
     });
     
@@ -55,12 +59,19 @@ export default class Game {
   }
 
   render (context) {
-    this.player.draw(context);
-    this.player.update();
-
-    this.waypoints.forEach(point => {
-      point.draw(context)
+    // first draw track segments
+    this.waypoints.forEach((point, index) => {
+      this.drawPath(index);
     })
+    
+    // then draw track waypoints on top
+    this.waypoints.forEach((point, index) => {
+      if (index % 2) point.draw(context);
+    })
+
+    // then update & draw player on top of that
+    this.player.update();
+    this.player.draw(context);
   }
 
   init () {
@@ -68,10 +79,38 @@ export default class Game {
     //   this.waypoints.push(new Waypoint(this));
     // }
 
+    // this.createRandomWaypoints();
+    this.createPathWaypoints();
+    console.log(this.waypoints);
+  }
+
+  drawPath (segmentIndex) {
+    
+    this.context.beginPath();
+    
+    this.context.lineWidth = 30;
+    this.context.strokeStyle = 'black';
+    this.context.setLineDash([]);
+
+    this.context.moveTo(this.waypoints[segmentIndex].origin.x, this.waypoints[segmentIndex].origin.y);
+    if(segmentIndex < this.waypoints.length - 1) {
+      this.context.lineTo(this.waypoints[segmentIndex + 1].origin.x, this.waypoints[segmentIndex + 1].origin.y)
+      if(segmentIndex < this.waypoints.length - 2) {
+        this.context.lineTo(this.waypoints[segmentIndex + 2].origin.x, this.waypoints[segmentIndex + 2].origin.y)
+      }
+    } else {
+      this.context.lineTo(this.waypoints[0].origin.x, this.waypoints[0].origin.y)
+    }
+    this.context.stroke();
+    
+  }
+
+  createRandomWaypoints () {
+    // create waypoints using 
     // circle packing brute force algo
     let attempts = 100;
     while (attempts > 0 && this.waypoints.length < this.numberOfWaypoints) {
-      let testWaypoint = new Waypoint(this);
+      let testWaypoint = new Waypoint(this, {});
       let overlap = false;
       this.waypoints.forEach(waypoint => {
         const dx = testWaypoint.origin.x - waypoint.origin.x;
@@ -88,7 +127,30 @@ export default class Game {
       attempts--;
       this.player.waypoints = [...this.waypoints];
     }
+  }
 
+  createPathWaypoints () {
+    const svg = document.querySelector('object');
+    let path = svg.contentDocument.querySelector('#trackpath-monza');
+    const points = Math.floor(path.getTotalLength());
+    
+
+    let stepSize = 30;
+    
+
+    for(let i=0; i<Math.floor(points / stepSize); i++) {
+      console.log('x:' + Math.floor(path.getPointAtLength(i * stepSize).x, 'y:' + Math.floor(path.getPointAtLength(i * stepSize).y)));
+
+      let pathWaypoint = new Waypoint(this, {
+        x: path.getPointAtLength(i * stepSize).x,
+        y: path.getPointAtLength(i * stepSize).y
+      })
+      this.waypoints.push(pathWaypoint);
+      
+    }
+    
+    console.log(this.waypoints);
+    this.player.waypoints = [...this.waypoints];
   }
 
 }
