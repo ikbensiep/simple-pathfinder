275024b2ca4471f2a921b31dd8e201e10f4afe75
ikbensiep <ikbensiep>
2023-09-24 01:11:35 +0200

    add wp origin arg, update waypoint draw styles

--- js/modules/waypoint.js
+++ js/modules/waypoint.js
@@ -1,17 +1,17 @@
 export default class Waypoint {
-  constructor(game) {
+  constructor(game, origin) {
     this.game = game;
     this.isTarget = false;
 
     // origin is set in game#init, so 
     // might as well initialize to 0,0?
     this.origin = {
-      x: (Math.random() * this.game.width), 
-      y: (Math.random() * this.game.height)
+      x: origin.x || (Math.random() * this.game.width), 
+      y: origin.y || (Math.random() * this.game.height)
     };
     
     // draw style for waypoint //
-    this.collisionRadius = 40;
+    this.collisionRadius = 10;
     this.strokeStyle = 'orange';
     this.fillStyle = 'white';
 
@@ -21,12 +21,17 @@ export default class Waypoint {
 
   draw (context) {
 
-    context.lineWidth = 5;
+    context.lineWidth = 2;
     context.strokeStyle = this.strokeStyle;
-    if(this.isTarget) context.strokeStyle = 'red';
-    for (let i=1; i<4; i++) {
+    this.fillStyle = 'white'
+    if(this.isTarget) { 
+      context.strokeStyle = 'red';
+      this.fillStyle = 'red';
+    } 
+    for (let i=1; i<Math.floor(this.collisionRadius / 5); i++) {
+      context.setLineDash([]);
       context.beginPath();
-      context.arc(this.origin.x,this.origin.y,this.collisionRadius - i * 10,0,Math.PI * 2);
+      context.arc(this.origin.x, this.origin.y, this.collisionRadius - i * 5, 0,Math.PI * 2);
       context.save();
         context.globalAlpha = 0.75;
         context.fillStyle = this.fillStyle;
